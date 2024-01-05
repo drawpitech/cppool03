72a275d82ba328bd071d65501e5b4e3d6cd7bbd3
Clément <clement2104.boillot@gmail.com>
2024-01-05 09:48:58 +0100

    Add 5th task

--- Game.hs
+++ Game.hs
@@ -15,3 +15,18 @@ instance Show Item where
 
 data Mob = Mummy | Sketeleton Item | Witch (Maybe Item)
     deriving (Eq, Show)
+
+createMummy :: Mob
+createMummy = Mummy
+
+createArcher :: Mob
+createArcher = Sketeleton Bow
+
+createKnight :: Mob
+createKnight = Sketeleton Sword
+
+createWitch :: Mob
+createWitch = Witch Nothing
+
+createSorceress :: Mob
+createSorceress = Witch (Just MagicWand)
