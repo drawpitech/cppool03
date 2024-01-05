e4b69e713bf8610f5cf3e2cf2c43ef2effa882af
Clément <clement2104.boillot@gmail.com>
2024-01-05 11:05:37 +0100

    Add 12th task

--- Tree.hs
+++ Tree.hs
@@ -7,3 +7,9 @@
 
 data Tree a = Empty | Node (Tree a) a (Tree a)
     deriving (Show)
+
+addInTree :: Ord a => a -> Tree a -> Tree a
+addInTree value Empty = Node Empty value Empty
+addInTree value (Node left root right)
+    | value < root = Node (addInTree value left) root right
+    | value > root = Node left root (addInTree value right)
