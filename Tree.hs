a205ad96a6a7d6fd63d2151cf7b0310808ee61aa
Clément <clement2104.boillot@gmail.com>
2024-01-05 11:38:43 +0100

    Add 13th task

--- Tree.hs
+++ Tree.hs
@@ -13,3 +13,7 @@ addInTree value Empty = Node Empty value Empty
 addInTree value (Node left root right)
     | value < root = Node (addInTree value left) root right
     | value > root = Node left root (addInTree value right)
+
+instance Functor Tree where
+    fmap _ Empty = Empty
+    fmap f (Node left root right) = Node (fmap f left) (f root) (fmap f right)
